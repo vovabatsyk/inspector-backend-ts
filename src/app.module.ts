0a1a85e9201aa196ca0bf77dc220abd7b37239d3
vovabatsyk <vovabatsyk@gmail.com>
2022-05-10 18:28:00 +0300

    create & getAll users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,27 @@
+import { SequelizeModule } from '@nestjs/sequelize'
 import { Module } from '@nestjs/common'
+import { UsersModule } from './users/users.module'
+import { ConfigModule } from '@nestjs/config'
+import { User } from './users/user.model'
 
-@Module({})
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mariadb',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
 export class AppModule {}
