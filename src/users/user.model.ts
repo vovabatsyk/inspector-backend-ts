6541376c0cf50c70b6a26502d3cc3f97d23b782b
vovabatsyk <vovabatsyk@gmail.com>
2022-05-10 21:49:05 +0300

    add swagger

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Model, Table, Column, DataType } from 'sequelize-typescript'
 
 interface UserCreationAttrs {
@@ -8,21 +9,27 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Ідентифікатор користувача' })
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number
 
+  @ApiProperty({ example: 'user@gmail.com', description: 'Електронна скринька' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string
 
+  @ApiProperty({ example: 'qwerty1234', description: 'Пароль користувача' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string
 
+  @ApiProperty({ example: 'Володимир Васильович Зенько', description: 'П.І.Б' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   username: string
 
+  @ApiProperty({ example: 'false', description: 'Заблокувати користувача' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean
 
+  @ApiProperty({ example: 'Спам', description: 'Причина блокування користувача' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { CreateUserDto } from './dto/create-user-dto'
+import { User } from './user.model'
 import { UsersService } from './users.service'
 
-@Controller('users')
+@ApiTags('Користувачі')
+@Controller('api/users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @ApiOperation({ summary: 'Створити користувача' })
+  @ApiResponse({ status: 200, type: User })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto)
   }
 
+  @ApiOperation({ summary: 'Всі користувачі' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   getAll() {
     return this.userService.getAllUsers()
