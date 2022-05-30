3ddb64614d886db0bbd6c5c1a8d2970d378dbb94
vovabatsyk <vovabatsyk@gmail.com>
2022-05-30 11:51:54 +0300

    add auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,11 +15,17 @@ export class UsersService {
     const user = await this.userRepository.create(dto)
     const role = await this.roleService.getRoleByValue('street')
     await user.$set('roles', [role.id])
+    user.roles = [role]
     return user
   }
 
   async getAllUsers() {
     const users = await this.userRepository.findAll({ include: { all: true } })
     return users
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email }, include: { all: true } })
+    return user
   }
 }
