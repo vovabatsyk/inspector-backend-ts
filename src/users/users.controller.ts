2a1086f4f3b4a2cd16e91121d0a43d8897ea5f52
vovabatsyk <vovabatsyk@gmail.com>
2022-05-30 13:06:47 +0300

    add roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { Roles } from 'src/auth/roles-auth.decorator'
+import { RolesGuard } from 'src/auth/roles.guard'
+import { AddRoleDto } from './dto/add-role.dto'
+import { BanUserDto } from './dto/ban-user.dto'
 import { CreateUserDto } from './dto/create-user-dto'
 import { User } from './user.model'
 import { UsersService } from './users.service'
@@ -18,8 +23,31 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Всі користувачі' })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   @Get()
   getAll() {
     return this.userService.getAllUsers()
+  }
+
+  @ApiOperation({ summary: 'Змінити тип користувача' })
+  @ApiResponse({ status: 200 })
+  @UseGuards(JwtAuthGuard)
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() dto: AddRoleDto) {
+    return this.userService.addRole(dto)
+  }
+
+  @ApiOperation({ summary: 'Заблокувати користувача' })
+  @ApiResponse({ status: 200 })
+  @UseGuards(JwtAuthGuard)
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  ban(@Body() dto: BanUserDto) {
+    return this.userService.ban(dto)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { RolesService } from 'src/roles/roles.service'
+import { AddRoleDto } from './dto/add-role.dto'
+import { BanUserDto } from './dto/ban-user.dto'
 import { CreateUserDto } from './dto/create-user-dto'
 import { User } from './user.model'
 
@@ -13,7 +15,7 @@ export class UsersService {
 
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto)
-    const role = await this.roleService.getRoleByValue('street')
+    const role = await this.roleService.getRoleByValue('admin')
     await user.$set('roles', [role.id])
     user.roles = [role]
     return user
@@ -28,4 +30,16 @@ export class UsersService {
     const user = await this.userRepository.findOne({ where: { email }, include: { all: true } })
     return user
   }
+
+  async addRole(dto: AddRoleDto) {
+    const user = await this.userRepository.findByPk(dto.userId)
+    const role = await this.roleService.getRoleByValue(dto.value)
+    if (role && user) {
+      await user.$add('role', role.id)
+      return dto
+    }
+    throw new HttpException('Користувач або тип не знайдено', HttpStatus.NOT_FOUND)
+  }
+
+  async ban(dto: BanUserDto) {}
 }
